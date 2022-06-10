8c731b93ce6ee2d883addf5d110b7b7d9d966eaa
Wyatt Kirby <wyatt@apsis.io>
2022-06-10 14:48:55 -0400

    Add release for arm64

--- bin/releases.sh
+++ bin/releases.sh
@@ -9,6 +9,9 @@ mkdir -p releases/$VERSION
 cargo build --release
 cp target/release/runner releases/$VERSION/runner-$VERSION-x86_64-apple-darwin
 
+cargo build --target=aarch64-apple-darwin --release
+cp target/release/runner releases/$VERSION/runner-$VERSION-aarch64-apple-darwin
+
 rm -rf target
 docker build --tag runner-build:latest .
 docker run -v $(pwd):/app runner-build:latest cargo build --release --target x86_64-unknown-linux-musl
